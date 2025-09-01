5e004c5ca506b1a958c35c52bc7c8dea979d6840
pierdlcr2006 <dpierofabrizzio@icloud.com>
2025-09-01 15:08:15 -0500

    modificacion en clase studentcontroller

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -2,6 +2,9 @@ package controllers;
 
 public class StudentController {
     public static void main(String[] args) {
+
         System.out.println("Archivo modificado por ANTONY CHOLÁN");
+        System.out.println("Se agrego esta linea por Piero De la Cruz");
     }
+
 }
